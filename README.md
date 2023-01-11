7c862f5834d7cff6209423034e024f167bbc5864
GiadaMarzapane <giada.ortesta@gmail.com>
2023-01-11 16:41:43 +0100

    Esercizio completato

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 //ESERCIZIO GRID CAMPO MINATO
-1- L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-2- Ogni cella ha un numero progressivo, da 1 a 100.
+OK // 1- L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+OK // 2- Ogni cella ha un numero progressivo, da 1 a 100.
     - Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-3- Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+OK // 3- Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
 const play = document.getElementById('play');
-const container = document.querySelector('.container');
+const container = document.querySelector('.container');    
 
-function numbers(){
-    for (let index = 1; index <= 100; index++) {
-        const element = index;
-        
-        const myCell = document.createElement('div');
-        myCell.innerHTML += element;
-        myCell.classList.add('my-cell');
-        container.append(myCell);
-    }
-}
+for (let index = 1; index <= 100; index++) {
+    const element = index;
 
-play.addEventListener('click',
+    const myCell = document.createElement('div');
+    myCell.innerHTML += element;
+    myCell.classList.add('my-cell');
+    container.append(myCell);
+    
+    play.addEventListener('click',
+        function(){
+    
+            container.classList.remove('hidden');
+            container.classList.add('flex');
+        }
+    )
+    myCell.addEventListener('click',
     function(){
-
-        container.classList.remove('hidden');
-        container.classList.add('flex');
-        numbers();
+        myCell.classList.add('blu');
+        
+        console.log('Numero cliccato', myCell.innerHTML);
     }
-)
+    )
+}
+        
