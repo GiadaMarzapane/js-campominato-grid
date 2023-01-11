0a0ae128512b90cee15294d3b6668ade61da50bb
GiadaMarzapane <giada.ortesta@gmail.com>
2023-01-11 18:07:37 +0100

    Add bonus

---
+++ BONUS/js/script.js
@@ -0,0 +1,91 @@
+const play = document.getElementById('play');
+const container = document.querySelector('.container');
+const myChoice = document.getElementById('my-choice');
+
+play.addEventListener('click',
+
+    function(){
+
+        if (myChoice.value == 'easy'){
+            easyLevel()
+            
+        }
+        else if (myChoice.value == 'medium'){
+            mediumLevel()
+        }
+        else if (myChoice.value == 'hard') {
+            hardLevel()
+        }
+    }
+
+)
+
+
+function easyLevel() {
+    for (let index = 1; index <= 100; index++) {
+        const element = index;
+
+        const myCell = document.createElement('div');
+        myCell.innerHTML += element;
+        myCell.classList.add('my-cell');
+        myCell.classList.add('easy');
+        container.append(myCell);
+
+        container.classList.remove('hidden');
+        container.classList.add('flex');      
+
+        myCell.addEventListener('click',
+            function () {
+                myCell.classList.add('blu');
+
+                console.log('Numero cliccato', myCell.innerHTML);
+            }
+        )
+    }   
+}
+
+function mediumLevel() {
+    for (let index = 1; index <= 81; index++) {
+        const element = index;
+
+        const myCell = document.createElement('div');
+        myCell.innerHTML += element;
+        myCell.classList.add('my-cell');
+        myCell.classList.add('medium');
+        container.append(myCell);        
+
+        container.classList.remove('hidden');
+        container.classList.add('flex');
+                    
+        myCell.addEventListener('click',
+            function () {
+                myCell.classList.add('blu');
+
+                console.log('Numero cliccato', myCell.innerHTML);
+            }
+        )
+    }
+}
+
+function hardLevel() {
+    for (let index = 1; index <= 49; index++) {
+        const element = index;
+
+        const myCell = document.createElement('div');
+        myCell.innerHTML += element;
+        myCell.classList.add('my-cell');
+        myCell.classList.add('hard');
+        container.append(myCell);
+
+        container.classList.remove('hidden');
+        container.classList.add('flex');
+
+        myCell.addEventListener('click',
+            function () {
+                myCell.classList.add('blu');
+
+                console.log('Numero cliccato', myCell.innerHTML);
+            }
+        )
+    }
+}
